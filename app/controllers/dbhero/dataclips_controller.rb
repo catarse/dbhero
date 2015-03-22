d4dcc8699a51c7ffb85f221f775c353f4ac5ae77
Antônio Roberto <forevertonny@gmail.com>
2015-03-22 20:43:22 -0300

    use check_auth to look on authenticate configurations

--- app/controllers/dbhero/dataclips_controller.rb
+++ app/controllers/dbhero/dataclips_controller.rb
@@ -2,16 +2,16 @@ require_dependency "dbhero/application_controller"
 
 module Dbhero
   class DataclipsController < ApplicationController
+    before_action :check_auth, except: [:show]
     before_action :set_dataclip, only: [:show, :edit, :update, :destroy]
     respond_to :html, :csv
 
-    # GET /dataclips
     def index
       @dataclips = Dataclip.all
     end
 
-    # GET /dataclips/1
     def show
+      check_auth if @dataclip.private?
       @dataclip.query_result
 
       respond_to do |format|
@@ -22,29 +22,24 @@ module Dbhero
       end
     end
 
-    # GET /dataclips/new
     def new
       @dataclip = Dataclip.new
     end
 
-    # GET /dataclips/1/edit
     def edit
       @dataclip.check_query
     end
 
-    # POST /dataclips
     def create
       @dataclip = Dataclip.create(dataclip_params)
       respond_with @dataclip, location: edit_dataclip_path(@dataclip),notice: 'Dataclip was successfully created.'
     end
 
-    # PATCH/PUT /dataclips/1
     def update
       @dataclip.update(dataclip_params)
       respond_with @dataclip, location: edit_dataclip_path(@dataclip), notice: 'Dataclip was successfully updated.'
     end
 
-    # DELETE /dataclips/1
     def destroy
       @dataclip.destroy
       redirect_to dataclips_url, notice: 'Dataclip was successfully destroyed.'
@@ -59,6 +54,19 @@ module Dbhero
       # Only allow a trusted parameter "white list" through.
       def dataclip_params
         params.require(:dataclip).permit(:description, :raw_query)
+      end
+
+      def check_auth
+        if Dbhero.authenticate_method.present?
+          send(Dbhero.authenticate_method)
+        end
+      end
+
+      def user_representation
+        if Dbhero.current_user_method.present?
+          user = send(Dbhero.current_user_method)
+          user.send(Dbhero.user_representation) if user
+        end
       end
   end
 end

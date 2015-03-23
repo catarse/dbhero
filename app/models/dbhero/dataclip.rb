b8e1afedcf99887b2e2cf32db1f52e5cabdc7dc5
Antônio Roberto <forevertonny@gmail.com>
2015-03-22 21:10:26 -0300

    remove limit from query_result

--- app/models/dbhero/dataclip.rb
+++ app/models/dbhero/dataclip.rb
@@ -21,7 +21,7 @@ module Dbhero
     def query_result
       Dataclip.transaction do
         begin
-          @q_result ||= ActiveRecord::Base.connection.select_all("select sub.* from (#{self.raw_query}) sub limit 1000")
+          @q_result ||= ActiveRecord::Base.connection.select_all("select sub.* from (#{self.raw_query}) sub")
         rescue => e
           self.errors.add(:base, e.message)
         end
